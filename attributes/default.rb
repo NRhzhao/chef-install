50d1d94c0d22197f9b62f32b25997f93d9508cbd
hang zhao <hzhao@newrelic.com>
2023-01-17 14:37:58 -0800

    init

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+default["newrelic_install"]["env"]["api_key"] = nil
+default["newrelic_install"]["env"][["account_id"] = nil
+default["newrelic_install"]["env"][["region"] = "US"
+default["newrelic_install"]["env"]["proxy_url"] = nil
+default["newrelic_install"][["install_names"] = []
+default["newrelic_install"]["tags"] = []
+default["newrelic_install"]["vebosity"] = "info"
+default["newrelic_install"]["timeout_seconds"] = 600
+
